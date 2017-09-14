d347cc95bf59b0fd24530a86b5a2fa2a7193421c
yicheng <2396611405@qq.com>
2017-09-14 16:30:20 +0800

    no message

--- headerPCH.swift
+++ headerPCH.swift
@@ -8,4 +8,4 @@
 
 import Foundation
 let  API_TEST = 0 //0--正式服务器  1--测试服务器
-let kBaseUrlApi = ((API_TEST == 0) ? "http://5d8.yicheng120.com/api/" :"http://test.5d8.yicheng120.com/api/")
+let kBaseUrlApi = "http://test.5d8.yicheng120.com/api/"
